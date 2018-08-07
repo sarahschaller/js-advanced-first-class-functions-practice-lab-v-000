5ad1d516deca415e0126fda0a2c5ead9d7bac883
Sarah Schaller <sarahcschaller@gmail.com>
2018-08-07 22:50:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const logDriversByHometown = function(drivers, hometown) {
   drivers.forEach(function(driver) {
     if (driver.hometown === hometown) {
       console.log(driver.name);
+    }
     });
   };
 };

b3bbf529ac66c99f52b6f545508e106490e3254c
Sarah Schaller <sarahcschaller@gmail.com>
2018-08-07 22:46:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,32 +4,32 @@ const logDriverNames = function(drivers) {
   };
 };
 
-// const logDriversByHometown = function(drivers, hometown) {
-//   drivers.forEach(function(driver)) {
-//     if (driver.hometown === hometown) {
-//       console.log(driver.name);
-//     };
-//   };
-// };
-//
-// const driversByRevenue = function(drivers) {
-//   return drivers.slice().sort(function(firstDriver, secondDriver) {
-//     return firstDriver.revenue - secondDriver.revenue;
-//   });
-// };
-//
-// const driversByName = function(drivers) {
-//   return drivers.slice().sort(function(firstDriver, secondDriver) {
-//     return firstDriver.namelocaleCompare(secondDriver.name);
-//   });
-// };
-//
-// const totalRevenue = function(drivers) {
-//   return drivers.reduce(function(total, driver) {
-//     return total + driver.revenue
-//   }, 0);
-// };
-//
-// const averageRevenue = function(drivers) {
-//   return totalRevenue(drivers) / drivers.length
-// };
+const logDriversByHometown = function(drivers, hometown) {
+  drivers.forEach(function(driver)) {
+    if (driver.hometown === hometown) {
+      console.log(driver.name);
+    };
+  };
+};
+
+const driversByRevenue = function(drivers) {
+  return drivers.slice().sort(function(firstDriver, secondDriver) {
+    return firstDriver.revenue - secondDriver.revenue;
+  });
+};
+
+const driversByName = function(drivers) {
+  return drivers.slice().sort(function(firstDriver, secondDriver) {
+    return firstDriver.namelocaleCompare(secondDriver.name);
+  });
+};
+
+const totalRevenue = function(drivers) {
+  return drivers.reduce(function(total, driver) {
+    return total + driver.revenue
+  }, 0);
+};
+
+const averageRevenue = function(drivers) {
+  return totalRevenue(drivers) / drivers.length
+};

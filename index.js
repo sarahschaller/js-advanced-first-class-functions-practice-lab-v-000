f94fa0c39c3b1b7864b053983d77de4292da9bda
Sarah Schaller <sarahcschaller@gmail.com>
2018-08-07 22:46:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const logDriverNames = function(drivers) {
-  drivers.forEach(function(driver)){
-    console.log(driver.name);
-  };
-};
-
-const logDriversByHometown = function(drivers, hometown) {
-  drivers.forEach(function(driver)) {
-    if (driver.hometown === hometown) {
-      console.log(driver.name);
-    };
-  };
-};
+// const logDriverNames = function(drivers) {
+//   drivers.forEach(function(driver)){
+//     console.log(driver.name);
+//   };
+// };
+//
+// const logDriversByHometown = function(drivers, hometown) {
+//   drivers.forEach(function(driver)) {
+//     if (driver.hometown === hometown) {
+//       console.log(driver.name);
+//     };
+//   };
+// };
 
 const driversByRevenue = function(drivers) {
   return drivers.slice().sort(function(firstDriver, secondDriver) {

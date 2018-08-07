a9651edb3448717ae7413fe96ef5ea623241cc77
Sarah Schaller <sarahcschaller@gmail.com>
2018-08-07 22:47:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,13 +4,13 @@
 //   };
 // };
 //
-// const logDriversByHometown = function(drivers, hometown) {
-//   drivers.forEach(function(driver)) {
-//     if (driver.hometown === hometown) {
-//       console.log(driver.name);
-//     };
-//   };
-// };
+const logDriversByHometown = function(drivers, hometown) {
+  drivers.forEach(function(driver)) {
+    if (driver.hometown === hometown) {
+      console.log(driver.name);
+    };
+  };
+};
 
 const driversByRevenue = function(drivers) {
   return drivers.slice().sort(function(firstDriver, secondDriver) {
